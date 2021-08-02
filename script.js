930b89849701e0f29c2ba309b58d2abfe3a78948
Biz Hight <elizabeth.biz.hight@gmail.com>
2021-08-02 11:41:45 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,112 @@
+import Deck from './Deck.js'
+
+const CARD_VALUE_MAP = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14
+}
+
+const computerCardSlot = document.querySelector('.computer-card-slot')
+const playerCardSlot = document.querySelector('.player-card-slot')
+const computerDeckElement = document.querySelector('.computer-deck')
+const playerDeckElement = document.querySelector('.player-deck')
+const message = document.querySelector('.message')
+
+let playerDeck, computerDeck, inRound, stop
+
+document.addEventListener('click', () => {
+    if (stop) {
+        startGame()
+        return
+    }
+
+    if (inRound) {
+        cleanBeforeRound()
+    } else {
+        flipCards()
+    }
+})
+
+startGame()
+
+function startGame() {
+    const deck = new Deck()
+    deck.shuffle()
+
+    const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
+    playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+    computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+    inRound = false
+    stop = false
+
+    console.log(playerDeck)
+    console.log(computerDeck)
+
+    cleanBeforeRound()
+}
+
+function cleanBeforeRound() {
+    inRound = false
+    computerCardSlot.innerHTML = ''
+    playerCardSlot.innerHTML = ''
+    message.innerText = ''
+    
+    updateDeckCount()
+}
+
+function flipCards() {
+    inRound = true
+
+    const playerCard = playerDeck.pop()
+    const computerCard = computerDeck.pop()
+
+    playerCardSlot.appendChild(playerCard.getHTML())
+    computerCardSlot.appendChild(computerCard.getHTML())
+
+    updateDeckCount()
+
+    if (isRoundWinner(playerCard, computerCard)) {
+        message.innerText = "WIN!"
+        playerDeck.push(playerCard)
+        playerDeck.push(computerCard)
+    } else if (isRoundWinner(computerCard, playerCard)) {
+        message.innerText = "lose"
+        computerDeck.push(playerCard)
+        computerDeck.push(computerCard)
+    } else {
+        message.innerText = "Draw"
+        playerDeck.push(playerCard)
+        computerDeck.push(computerCard)
+    }
+
+    if (isGameOver(playerDeck)) {
+        message.innerText = "GAME OVER! You lose."
+        stop = true
+    } else if (isGameOver(computerDeck)) {
+        message.innerText = "GAME OVER! You win!!!"
+        stop = true
+    }
+}
+
+function updateDeckCount() {
+    computerDeckElement.innerText = computerDeck.numberOfCards
+    playerDeckElement.innerText = playerDeck.numberOfCards
+}
+
+function isRoundWinner(cardOne, cardTwo) {
+    return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value]
+}
+
+function isGameOver(deck) {
+    return deck.numberOfCards === 0
+}
